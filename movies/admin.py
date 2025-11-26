cd3effc3e7491c81df36aefe80a272312dda7b2a
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-11-26 13:39:03 +0200

    add movie, screen, theater models and add to admin page

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Movie, Theater, Screen
+
+
 # Register your models here.
+class ScreenInline(admin.TabularInline):
+    model = Screen
+    extra = 1
+
+
+@admin.register(Theater)
+class TheaterAdmin(admin.ModelAdmin):
+    inlines = [ScreenInline]
+    list_display = ("name", "city")
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "release_date", "duration", "is_active")
+    search_fields = ("title", "genre", "release_data")
+
+
+@admin.register(Screen)
+class ScreenAdmin(admin.ModelAdmin):
+    list_display = ("name", "theater", "capacity", "screen_type")
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    rating = models.ValueRange(1, 5)
+    poster = models.ImageField(upload_to="images/", blank=True, null=True)
+    trailer_url = models.URLField(blank=True, null=True)
+    release_date = models.DateField()
+    is_active = models.BooleanField(
+        default=True, help_text="Is this movie currently showing?"
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Theater(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name} at {self.city}"
+
+
+class Screen(models.Model):
+    SCREEN_TYPE_CHOICES = [
+        ("2D", "Standard 2D"),
+        ("3D", "3D"),
+        ("IMAX", "IMAX"),
+    ]
+
+    name = models.CharField(max_length=50)
+    theater = models.ForeignKey(
+        Theater, on_delete=models.CASCADE, related_name="screens"
+    )
+    capacity = models.PositiveIntegerField(help_text="Total number of seats")
+    screen_type = models.CharField(max_length=10, choices=SCREEN_TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} at {self.theater}"
